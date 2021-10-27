6df45cc7c8e3343956f1487853dfddfda9451af4
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-10-27 15:38:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,68 +1,53 @@
 console.log('%c HI', 'color: firebrick')
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-const breedDropDown = document.getElementById("breed-dropdown")
-let breeds = []
-
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetchImages()
-    fetchBreeds()
+  loadImages();
+  loadBreeds();
 })
 
-function fetchImages() {
-    return fetch(imgUrl)
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+  return fetch(imgUrl)
     .then(resp => resp.json())
-    .then(json => {
-        // console.log(json)
-        renderImages(json.message)
-    })
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
 }
 
-function renderImages(images) {
-    // console.log(images)
-    const imageContainer = document.getElementById("dog-image-container")
-    images.forEach(image => {
-    // console.log(image)
-    const img = document.createElement('img')
-    img.src = image
-    imageContainer.appendChild(img)
-})}
+function addImage(picUrl) {
+  const container = document.getElementById("dog-image-container");
+  const newImage = document.createElement('img');
+  newImage.src = picUrl;
+  container.appendChild(newImage);
+}
 
-function fetchBreeds() {
-    return fetch(breedUrl)
+function loadBreeds() {
+  return fetch('https://dog.ceo/api/breeds/list/all')
     .then(resp => resp.json())
-    .then(json => {
-        // console.log(json)
-        const breeds = Object.keys(json.message)
-        updateBreeds(breeds)
-        renderFilteredBreeds()
-    })
-}
- 
-function updateBreeds(breeds) {
-    const ul = document.getElementById("dog-breeds")
-    breeds.forEach(breed => {
-        const li = document.createElement('li')
-        li.innerText = breed
-        ul.appendChild(li)
-        li.addEventListener("click", function(event) {
-            event.target.style.color = "blue"
-        })
-    })
+    .then(results => {
+      const breeds = Object.keys(results.message);
+      addBreeds(breeds);
+
+    });
 }
 
-function renderFilteredBreeds() {
-    let dropDown = document.getElementById("breed-dropdown")
-    console.log(document)
-    dropDown.addEventListener("change", function(event) {
-        fetchFilteredBreeds(event.target.value)
-    })
+function addBreeds(breeds) {
+  const ul = document.getElementById("dog-breeds");
+  let dropDown = document.getElementById('breed-dropdown');
+  breeds.forEach(breed => {
+    const li = document.createElement("li");
+    li.innerText = breed;
+    ul.appendChild(li);
+    li.addEventListener("click", function(event) {
+      event.target.style.color = "blue";
+    });
+  });
 }
 
-// associate a constant with the array of dog breeds
-function fetchFilteredBreeds(letter) { 
-    updateBreeds(breeds.filter(breed => breed.startswith(letter)))
-}
-
-// call on renderFilteredBreeds()
+// associate variable with DOM element with id breed-dropdown
+// let dropDown = document.findElementById('breed-dropdown');
+// add change event listener to the variable 
+// dropDown.addEventListener('click', funciton(event) {
+// addBreeds(breeds.startswith.event.value)
+// })
